b15cb798f4baa1bd8d867aa72ead99275623b562
zac4312 <sushi.ninja315@gmail.com>
2026-06-22 16:18:01 +0000

    ??

--- perm.sh
+++ perm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo rsync -avH --exclude='~/PROJECTS/cmd/.git' ~/PROJECTS/cmd /usr/local/bin
+
 cd /usr/local/bin
 ls
 while true; do
+echo "Make Command executable: "
 echo "enter !!stop@ to stop: "
 read -p "chmod +x: " choice
 if [[ $choice == "!!stop@"  ]]; then
